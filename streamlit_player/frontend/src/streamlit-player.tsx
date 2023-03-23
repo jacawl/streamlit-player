084deb0c0b48f1ab9257dc047818da19344ee2f3
jacawl <jcawl321@gmail.com>
2023-03-23 14:31:55 -0400

    add button to seek through timestamps

--- streamlit_player/frontend/src/streamlit-player.tsx
+++ streamlit_player/frontend/src/streamlit-player.tsx
@@ -10,6 +10,13 @@ import HeightObserver from "./height-observer"
 
 const StreamlitPlayer = ({ args }: ComponentProps) => {
   const [playerEvents, setPlayerEvents] = useState({})
+  const [tracker, setTracker] = useState(0)
+  let segments = args.timestamps
+  let player: any
+
+  function ref(playerInstance: any) {
+    player = playerInstance;
+  }
 
   // Handle events
   useEffect(() => {
@@ -27,9 +34,18 @@ const StreamlitPlayer = ({ args }: ComponentProps) => {
     setPlayerEvents(events)
   }, [args.events])
 
+  function time() {
+    // const currentTime = player.getCurrentTime();
+    player.seekTo(segments[tracker], 'seconds')
+    setTracker(tracker + 1)
+    if (tracker == segments.length - 1)
+      setTracker(0)
+  }
+
   return (
     <HeightObserver onChange={Streamlit.setFrameHeight} fixedHeight={args.height}>
       <ReactPlayer
+        ref={ref}
         url={args.url}
         width="100%"
         height={args.height || undefined}
@@ -45,6 +61,7 @@ const StreamlitPlayer = ({ args }: ComponentProps) => {
         config={args.config || undefined}
         {...playerEvents}
       />
+      <button onClick={time}>foward</button>
     </HeightObserver>
   )
 }
